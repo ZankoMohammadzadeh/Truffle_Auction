0b17d8e059ddb637a5048504880f5bc32335bad3
zanko <zanko8517@yahoo.com>
2022-03-13 00:46:09 +0330

    Auction progress

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -2,6 +2,30 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Auction {
-  constructor() public {
+  address payable proficienary;
+  uint endingTime;
+  address highestBidder;
+  uint highestBid;
+  mapping (address => uint) remainedBid;
+
+  event highestBidding(address, uint);
+
+  constructor(address payable _proficienary, uint _EndingTime) {
+    proficienary = _proficienary;
+    endingTime = block.timestamp + _EndingTime;
   }
+
+  function bid() payable public{
+    require(endingTime <= block.timestamp, 'Auction is already finished');
+    if(msg.value > highestBid){
+      if(highestBid != 0){
+        remainedBid[highestBidder] = highestBid;
+      }
+      highestBid = msg.value;
+      highestBidder = msg.sender;
+      emit highestBidding(highestBidder, highestBid);
+    }
+  }
+
+  
 }
