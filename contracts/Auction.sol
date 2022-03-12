adf0bcdd2c5c8c1b9f12e0aa7248f3cf39f34a61
zanko <zanko8517@yahoo.com>
2022-03-13 01:26:06 +0330

    auction progress

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -2,30 +2,39 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Auction {
-  address payable proficienary;
-  uint endingTime;
+  address payable proficiary;
+  uint auctionEndingtime;
   address highestBidder;
   uint highestBid;
-  mapping (address => uint) remainedBid;
+  mapping (address => uint) pendingBids;
 
-  event highestBidding(address, uint);
+  event highestBidding(address bidder, uint value);
 
-  constructor(address payable _proficienary, uint _EndingTime) {
-    proficienary = _proficienary;
-    endingTime = block.timestamp + _EndingTime;
+  constructor(address payable _proficiary, uint _biddingTime) {
+    proficiary = _proficiary;
+    auctionEndingtime = block.timestamp + _biddingTime;
   }
 
-  function bid() payable public{
-    require(endingTime <= block.timestamp, 'Auction is already finished');
-    if(msg.value > highestBid){
-      if(highestBid != 0){
-        remainedBid[highestBidder] = highestBid;
+  function bid() payable external{
+    require(auctionEndingtime < block.timestamp, 'Auction already finished');
+    require(msg.value <= highestBid, 'Bid Not High Enough');
+    
+    if(highestBid != 0){
+      pendingBids[highestBidder] += highestBid;
+    }
+    highestBid = msg.value;
+    highestBidder = msg.sender;
+    emit highestBidding(highestBidder, highestBid);
+  }
+
+  function withdraw() external{
+    uint _value = pendingBids[msg.sender];
+
+    if(_value > 0){  
+      pendingBids[msg.sender] = 0;
+      if( payable(msg.sender).send(_value) == false){
+        pendingBids[msg.sender] = _value;
       }
-      highestBid = msg.value;
-      highestBidder = msg.sender;
-      emit highestBidding(highestBidder, highestBid);
     }
   }
-
-  
 }
